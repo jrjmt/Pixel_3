c262cd1bd511ee74d7b88d0849dbd17b1c2e4735
Mohammad Kibria <106745675+kibria5@users.noreply.github.com>
2023-12-07 20:39:51 +0600

    b1c1: Add speed preopt for NexusLauncher
    Signed-off-by: kibria5 <mdkibria687@gmail.com>

--- device.mk
+++ device.mk
@@ -801,6 +801,9 @@ endif
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI  # For AOSP
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    NexusLauncherRelease \
+    SettingsGoogle
 
 # Compile SystemUI on device with `speed`.
 PRODUCT_PROPERTY_OVERRIDES += \
